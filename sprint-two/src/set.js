7d0698be6b1c3784f4e253717ce963db0fdf54de
rhinodavid <dawalsh@gmail.com>
2016-09-15 16:27:51 -0700

    Complete implementation of set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[JSON.stringify(item)] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(JSON.stringify(item));
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    delete this._storage[JSON.stringify(item)];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add      = constant
+ contains = constant
+ remove   = constant 
  */
