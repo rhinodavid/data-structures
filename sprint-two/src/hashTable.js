3ad24040617f4c6cb55f8c74b208b80733c6dad0
rhinodavid <dawalsh@gmail.com>
2016-09-15 23:04:38 -0700

    Attempt to make array double in size
    Failing when the double function exits. Losing this._storage

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,17 +2,26 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._occupied = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // get whats in the bucket already
   var bucketContents = this._storage.get(index) || {};
+  if (!Object.getOwnPropertyNames(bucketContents).length) {
+    this._occupied++;
+  }
   // add my new value to the bucket
   bucketContents[k] = v;
   // put the whole thing back in the bucket
   this._storage.set(index, bucketContents);
+  // is the 75% rule exceeded??? if it is, we must grow.
+  if (this._occupied / this._limit >= 0.60 && arguments[2] !== 'growing') {
+    console.log('I am growing');
+    console.log(arguments[2]);
+    this._storage = this._grow();
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -28,6 +37,28 @@ HashTable.prototype.remove = function(k) {
   // get whats in the bucket already
   var bucketContents = this._storage.get(index);
   delete bucketContents[k];
+};
+
+HashTable.prototype._grow = function() {
+  console.dir(this);
+  // need to resize when 75% of buckets are occupied
+  this._limit = this._limit * 2;
+
+  // make a new storage that's double the size of the old storage
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(this._limit);
+  this._occupied = 0;
+
+  // go through old storage -- look in each bucket
+  oldStorage.each(function(bucket) {
+    for (var key in bucket) {
+      var value = bucket[key];
+      this.insert(key, value, 'growing');
+    }
+  }.bind(this));
+  debugger;
+  // losing this._storage once this function exits
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 };
 
 
