5dc60a8d6c333a4ede6f4c463cd02c8313783bf7
rhinodavid <dawalsh@gmail.com>
2016-09-17 11:41:32 -0700

    Implement hash function generator and bloom filter properties
    We went way, way too long between commits here

---
+++ sprint-two/src/bloomFilter.js
@@ -0,0 +1,46 @@
+var HashFunctionGenerator = function(k, max) {
+  this.k = k;
+  this.max = max;
+  this.bitwiseScrambleHash = function(shiftAmount, max, str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = (hash << shiftAmount) + hash + str.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+      hash = Math.abs(hash);
+    }
+    return hash % max;
+  }
+};
+
+// Hash Function Generator
+HashFunctionGenerator.prototype.generate = function() {
+  var k = this.k;
+  var max = this.max;
+  const result = [];
+  for (let i = 0; i < k; i++) {
+    result.push(this.bitwiseScrambleHash.bind(null, i, max));
+  }
+  return result;
+};
+
+var BloomFilter = function(k, max) {
+  this.k = k;
+  this.max = max;
+  this._storage = [];
+
+  for (let i = 0; i < this.max; i++) {
+    this._storage.push(false);
+  }
+
+  var hashMaker = new HashFunctionGenerator(this.k, this.max);
+  this._hashFunctions = hashMaker.generate();
+
+}
+
+BloomFilter.prototype.add = function(key) {
+  
+};
+
+BloomFilter.prototype.check = function(key) {
+
+};
